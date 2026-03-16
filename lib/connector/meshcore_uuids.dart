e49e80d3304decb48b58346445e6e7f1d6b3f757
just-stuff-tm <centretruckingllc@gmail.com>
2026-03-16 19:16:55 -0400

    style: format deviceNamePrefixes list for better readability

--- lib/connector/meshcore_uuids.dart
+++ lib/connector/meshcore_uuids.dart
@@ -3,5 +3,10 @@ class MeshCoreUuids {
   static const String rxCharacteristic = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
   static const String txCharacteristic = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
-  static const List<String> deviceNamePrefixes = ["MeshCore-", "Whisper-", "Wiscore-", "HT-"];
+  static const List<String> deviceNamePrefixes = [
+    "MeshCore-",
+    "Whisper-",
+    "Wiscore-",
+    "HT-",
+  ];
 }
